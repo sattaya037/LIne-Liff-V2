e8247047d354ef20c81c61ec962ab45a81ca932a
sattaya037 <sattaya.ra@ics-th.com>
2019-10-25 11:43:28 +0700

    make it better

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+var train = require('./train');
 
 app.use(express.static('public'));
 
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
+});
+app.get('/intent', function (req, res) {
+    res.json(train.intent());
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
---
+++ train.js
@@ -0,0 +1,24 @@
+var intent = [
+  {
+      "id": 1,
+      "message": "test",
+      "reply": "Gol D. Roger",
+  },
+  {
+      "id": 2,
+      "message": "test2",
+      "reply": "Sir Crocodile",
+  }
+  ];
+   
+  /* ฟังก์ชันสำหรับหา user ทั้งหมดในระบบ ในส่วนนี้ผมจะให้ส่งค่า users ทั้งหมดกลับไปเลย */
+  exports.intent = function() {
+      return intent;
+  };
+   
+  /* ฟังก์ชันสำหรับหา user จาก id ในส่วนนี้เราจะวน loop หา users ที่มี id ตามที่ระบุแล้วส่งกลับไป */
+  // exports.findById = function (id) {
+  //     for (var i = 0; i < users.length; i++) {
+  //         if (users[i].id == id) return users[i];
+  //     }
+  // };
